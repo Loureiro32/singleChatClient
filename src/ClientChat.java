8feadb1df04f96f77e1e50e67df2dd66e016a6de
Loureiro <mindera@C02Z689TLVCF-ME00547.local>
2024-12-19 12:26:06 +0000

    Live chat integration

--- src/ClientChat.java
+++ src/ClientChat.java
@@ -3,22 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientChat {
-    public void sendMessage(String message) throws IOException {
+    public void sendMessage() throws IOException {
         try {
-            Socket clientSocket = new Socket("localhost", 6000);
+            while(true) {
+                Socket clientSocket = new Socket("localhost", 6000);
 
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            out.println(message);
+                Scanner clientMessage = new Scanner(System.in);
+                System.out.println("Write your Message : ");
+                String message = clientMessage.nextLine();
+                out.println(message);
 
-            String response = in.readLine();
-            System.out.println(response);
+                String response = in.readLine();
+                System.out.println(response);
 
-            clientSocket.close();
-
+                clientSocket.close();
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         ClientChat clientChat = new ClientChat();
-        clientChat.sendMessage("Hello guys");
-        clientChat.sendMessage("Hello guys1");
+        clientChat.sendMessage();
     }
 }
